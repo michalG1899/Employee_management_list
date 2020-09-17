a72ccff44830e0e18d4295ccca7f7be0e2f471e3
Michal <gebski.michal@gmail.com>
2020-09-17 13:54:09 +0200

    First commit

---
+++ src/test/java/pl/projekt/employeeList/springboot2jpacrudexample/Springboot2JpaCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.projekt.employeeList.springboot2jpacrudexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2JpaCrudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
